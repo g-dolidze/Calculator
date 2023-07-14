3953f9dc02c7d351f242c1ef1407c2ce1cb80483
g-dolidze <giorgi.dolidze55@gmail.com>
2023-07-14 23:31:35 +0400

    function

--- CalculatorApp/src/App.tsx
+++ CalculatorApp/src/App.tsx
@@ -25,22 +25,97 @@ function reducer(state: any, { type, payload }: StateType) {
       const newDigit = { ...state, current: state.current + payload };
       return newDigit;
     case ACTIONS.CLEAR:
-      const clear = { ...state, current: payload };
+      const clear = {
+        ...state,
+        current: payload,
+        prev: payload,
+        operation: payload,
+      };
       return clear;
     case ACTIONS.DELETE_DIGIT:
-      const deletedDigit = { ...state, current: state.current.slice(0, -1) };
-      return deletedDigit;
-    case ACTIONS.CHOOSE_OPERATION:
-      const number = { ...state.current };
-      const clearItem = { ...state, current: "" };
-      const addPrev = { ...state, prev: number };
-      const prevNumber = {
-        ...state,
-        prev: addPrev,
-        current: clearItem,
-      };
+      if (state.current.length > 0) {
+        const deletedDigit = { ...state, current: state.current.slice(0, -1) };
+        return deletedDigit;
+      }
 
-      console.log(prevNumber);
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.operation === "+") {
+        return {
+          prev: Number(state.prev) + Number(state.current),
+          current: "",
+          operation: "+",
+        };
+      }
+      if (state.operation === "-") {
+        return {
+          prev: Number(state.prev) - Number(state.current),
+          current: "",
+          operation: "-",
+        };
+      }
+      if (state.operation === "/") {
+        return {
+          prev: Number(state.prev) / Number(state.current),
+          current: "",
+          operation: "/",
+        };
+      }
+      if (state.operation === "X") {
+        return {
+          prev: Number(state.prev) * Number(state.current),
+          current: "",
+          operation: "X",
+        };
+      } else {
+        return {
+          ...state,
+          prev: state.current,
+          current: "",
+          operation: payload,
+        };
+      }
+
+    case ACTIONS.EVALUATE:
+      if (state.operation === "+") {
+        return {
+          ...state,
+          current: Number(state.prev) + Number(state.current),
+          prev: "",
+          operation: "",
+        };
+      }
+      if (state.operation === "-") {
+        return {
+          ...state,
+          current: Number(state.prev) - Number(state.current),
+          prev: "",
+          operation: "",
+        };
+      }
+      if (state.operation === "/") {
+        return {
+          ...state,
+          current: Number(state.prev) / Number(state.current),
+          prev: "",
+          operation: "",
+        };
+      }
+      if (state.operation === "%") {
+        return {
+          ...state,
+          current: (Number(state.current) / Number(state.prev)) * 100,
+          prev: "",
+          operation: "",
+        };
+      }
+      if (state.operation === "X") {
+        return {
+          ...state,
+          current: Number(state.prev) * Number(state.current),
+          prev: "",
+          operation: "",
+        };
+      }
 
     default:
       return { ...initialState };
@@ -80,6 +155,13 @@ function App() {
       state: initialState,
     });
   };
+  const evaluateNumbers = () => {
+    dispatch({
+      type: ACTIONS.EVALUATE,
+      payload: operation,
+      state: initialState,
+    });
+  };
   return (
     <div className="page">
       <div className="calculator">
@@ -106,24 +188,26 @@ function App() {
           <button onClick={() => putNumber("7")}>7</button>
           <button onClick={() => putNumber("8")}>8</button>
           <button onClick={() => putNumber("9")}>9</button>
-          <button className="simbol" onClick={() => putNumber("x")}>
-            x
+          <button className="simbol" onClick={() => addOperation("X")}>
+            X
           </button>
           <button onClick={() => putNumber("4")}>4</button>
           <button onClick={() => putNumber("5")}>5</button>
           <button onClick={() => putNumber("6")}>6</button>
-          <button className="simbol" onClick={() => putNumber("-")}>
+          <button className="simbol" onClick={() => addOperation("-")}>
             -
           </button>
           <button onClick={() => putNumber("1")}>1</button>
           <button onClick={() => putNumber("2")}>2</button>
           <button onClick={() => putNumber("3")}>3</button>
-          <button className="simbol" onClick={() => putNumber("+")}>
+          <button className="simbol" onClick={() => addOperation("+")}>
             +
           </button>
           <button onClick={() => putNumber("0")}>0</button>
-          <button onClick={() => putNumber(",")}>,</button>
-          <button className="sum simbol">=</button>
+          <button onClick={() => putNumber(".")}>.</button>
+          <button className="sum simbol" onClick={evaluateNumbers}>
+            =
+          </button>
         </div>
       </div>
     </div>
